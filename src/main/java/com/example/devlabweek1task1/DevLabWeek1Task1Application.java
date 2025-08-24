3a2ba321916a67679fffe36858b6e595ed3482d8
Emin Səmədzadə <emin@example.com>
2025-08-24 14:45:17 +0400

    Initial commit

---
+++ src/main/java/com/example/devlabweek1task1/DevLabWeek1Task1Application.java
@@ -0,0 +1,13 @@
+package com.example.devlabweek1task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLabWeek1Task1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevLabWeek1Task1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devlabweek1task1/DevLabWeek1Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devlabweek1task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLabWeek1Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
